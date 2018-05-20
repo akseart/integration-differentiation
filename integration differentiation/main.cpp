507dd754e1c669221ca0b7d94d22ff03a9a1764c
akseart <akseart@ya.ru>
2018-05-20 17:43:23 +0300

    Initial Commit

---
+++ integration differentiation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  integration differentiation
+//
+//  Created by Артем on 20.05.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
